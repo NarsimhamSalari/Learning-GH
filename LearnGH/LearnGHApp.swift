e805032f87422dcc89abf4a2caca656c7cd5b4c9
Narsimham Salari <narsimham.salari.contractor@pepsico.com>
2022-08-01 18:04:47 -0500

    Initial Commit

---
+++ LearnGH/LearnGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnGHApp.swift
+//  LearnGH
+//
+//  Created by Narsimham Salari on 8/1/22.
+//
+
+import SwiftUI
+
+@main
+struct LearnGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
